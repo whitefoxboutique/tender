23d282df972f07044f06b2de6fa15b09a64af4a7
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-07-08 17:21:37 +1000

    mobile and desktop images in image block

--- extensions/image-block/src/Checkout.jsx
+++ extensions/image-block/src/Checkout.jsx
@@ -3,6 +3,8 @@ import {
   reactExtension,
   Image,
   useSettings,
+  Style,
+  View,
 } from '@shopify/ui-extensions-react/checkout';
 
 export default reactExtension(
@@ -11,9 +13,33 @@ export default reactExtension(
 );
 
 function Extension() {
-  const { image_url, alt } = useSettings();
+  const { image_url_d, image_url_m, alt } = useSettings();
 
   return (
-    <Image source={ image_url } accessibilityDescription={ alt } />
+    // <Image source={ image_url } accessibilityDescription={ alt } />
+    <View>
+      <View
+        maxInlineSize={
+          Style.default('fill')
+            .when({ viewportInlineSize: { min: 'medium' } }, '0')
+        }
+      >
+        <Image 
+          source={ image_url_d }
+          accessibilityDescription={ alt } 
+        />
+      </View>
+      <View
+        maxInlineSize={
+          Style.default('0')
+            .when({ viewportInlineSize: { min: 'medium' } }, 'fill')
+        }
+      >
+        <Image 
+          source={ image_url_m }
+          accessibilityDescription={ alt } 
+        />
+      </View>
+    </View>
   );
 }
