d2f4f2944e1dfb8a084204a12739178628fa5f4d
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-07-10 14:41:41 +1000

    fix logic error in sumArray util

--- utils.js
+++ utils.js
@@ -12,7 +12,12 @@ const arraySum = (arr, { pathToNumber } = {}) => {
     const pathNodes = pathToNumber.split('.');
       
     return arr.reduce((total, item) => {
-      const number = pathNodes.reduce((acc, part) => acc?.[part], obj);
+
+      const number = pathNodes.reduce((acc, part) => {
+        return acc && acc?.[part]; 
+      }, item);
+
+      // console.log('total', total, 'number', number, total + number);
       return total + number;
     }, 0);
   }
