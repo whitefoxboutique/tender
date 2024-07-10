112a166a06da2f86393e21733c57a9034daa7cfe
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-07-10 10:57:03 +1000

    initial points total calculation from metafields

--- extensions/loyalty-points-summary/src/Checkout.jsx
+++ extensions/loyalty-points-summary/src/Checkout.jsx
@@ -5,7 +5,11 @@ import {
   useCustomer,
   Link,
   useShop,
+  useAppMetafields,
+  useCartLines,
 } from '@shopify/ui-extensions-react/checkout';
+
+const { gidToId } = require('../../../utils');
 
 export default reactExtension(
   'purchase.checkout.cart-line-list.render-after',
@@ -15,6 +19,8 @@ export default reactExtension(
 function Extension() {
   const customer = useCustomer();
   const shop = useShop();
+  const metafields = useAppMetafields();
+  const cartLines = useCartLines();
 
   console.log('customer', customer);
   console.log('shop', shop);
@@ -22,7 +28,34 @@ function Extension() {
   const { storefrontUrl } = shop;
   const loginLink = `${ storefrontUrl }/account/login`;
 
-  const pointsTotal = 100;
+  const pointsMetafields = metafields.filter(mf => {
+    const { target, metafield } = mf;
+    const { type } = target;
+    const { namespace, key } = metafield;
+    return type === 'variant' && namespace == 'loyalty' && key === 'points';
+  });
+
+  const variantIdToPointsMap = pointsMetafields.reduce((map, mf) => {
+    const { target, metafield } = mf;
+    const { id } = target;
+    const { value } = metafield;
+    map[id] = value;
+    return map;
+  }, {});
+
+  console.log('variantIdToPointsMap', variantIdToPointsMap);
+
+  const pointsTotal = cartLines.reduce((total, line) => {
+    const variantGid = line?.merchandise?.id;
+    const variantId = gidToId(variantGid);
+
+    const { quantity } = line;
+
+    const points = variantIdToPointsMap[variantId] * quantity;
+    return total + points;
+  }, 0);
+
+  console.log('pointsTotal', pointsTotal);
 
   const debug = (
     <Banner>
