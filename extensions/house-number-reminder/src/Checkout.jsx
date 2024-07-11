a22505c3a286b768d54ece716c2f0b2ce8600d1c
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-07-11 15:26:02 +1000

    initial logic for showing banner on shipping address change

--- extensions/house-number-reminder/src/Checkout.jsx
+++ extensions/house-number-reminder/src/Checkout.jsx
@@ -1,7 +1,10 @@
 import {
   reactExtension,
   Banner,
+  useShippingAddress,
 } from "@shopify/ui-extensions-react/checkout";
+
+import * as allImported from '@shopify/ui-extensions-react/checkout';
 
 // 1. Choose an extension target
 export default reactExtension("purchase.checkout.delivery-address.render-after", () => (
@@ -9,7 +12,25 @@ export default reactExtension("purchase.checkout.delivery-address.render-after",
 ));
 
 function Extension() {
-  return (
-    <Banner>Hixxxx</Banner>
-  );
+  console.log('allImported', allImported);
+
+  const shippingAddress = useShippingAddress();
+
+  const { address1 } = shippingAddress;
+  
+  console.log('address1', address1);
+
+  if (!address1) {
+    // hasn't been entered yet
+    return;
+  }
+
+  const startsWithANumberRegex = /^\d/;
+  if (!startsWithANumberRegex.test(address1)) {
+    return (
+      <Banner>Hey babe, do you need to enter a house number?</Banner>
+    );
+  }
+
+  return;
 }
