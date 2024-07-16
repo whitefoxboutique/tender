c6435901b8732850f758f316af37e225ace90757
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-07-16 18:18:16 +1000

    sms consent initial components

--- extensions/custom-sms-consent/src/Checkout.jsx
+++ extensions/custom-sms-consent/src/Checkout.jsx
@@ -1,14 +1,10 @@
 import {
   reactExtension,
-  Banner,
-  BlockStack,
-  Checkbox,
-  Text,
   useApi,
-  useApplyAttributeChange,
-  useInstructions,
-  useTranslate,
-} from "@shopify/ui-extensions-react/checkout";
+  ConsentCheckbox,
+  ConsentPhoneField,
+  Banner,
+} from '@shopify/ui-extensions-react/checkout';
 
 // 1. Choose an extension target
 export default reactExtension('purchase.checkout.contact.render-after', () => (
@@ -16,44 +12,53 @@ export default reactExtension('purchase.checkout.contact.render-after', () => (
 ));
 
 function Extension() {
-  const translate = useTranslate();
-  const { extension } = useApi();
-  const instructions = useInstructions();
-  const applyAttributeChange = useApplyAttributeChange();
 
+  const { buyerIdentity } = useApi();
+  console.log('buyerIdentity', buyerIdentity);
 
-  // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
-  if (!instructions.attributes.canUpdateAttributes) {
-    // For checkouts such as draft order invoices, cart attributes may not be allowed
-    // Consider rendering a fallback UI or nothing at all, if the feature is unavailable
-    return (
-      <Banner title="Custom SMS Consent" status="warning">
-        {translate("attributeChangesAreNotSupported")}
-      </Banner>
-    );
-  }
-
-  // 3. Render a UI
   return (
-    <BlockStack border={"dotted"} padding={"tight"}>
-      <Banner title="Custom SMS Consent">
-        {translate("welcome", {
-          target: <Text emphasis="italic">{extension.target}</Text>,
-        })}
-      </Banner>
-      <Checkbox onChange={onCheckboxChange}>
-        {translate("iWouldLikeAFreeGiftWithMyOrder")}
-      </Checkbox>
-    </BlockStack>
-  );
+    <Banner>Hey
+      <ConsentCheckbox policy="sms-marketing">
+        Text me with news and offers
+      </ConsentCheckbox>
+      <ConsentPhoneField label="" policy="sms-marketing">
+      </ConsentPhoneField>
+    </Banner>
+  );  
 
-  async function onCheckboxChange(isChecked) {
-    // 4. Call the API to modify checkout
-    const result = await applyAttributeChange({
-      key: "requestedFreeGift",
-      type: "updateAttribute",
-      value: isChecked ? "yes" : "no",
-    });
-    console.log("applyAttributeChange result", result);
-  }
+
+  // // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
+  // if (!instructions.attributes.canUpdateAttributes) {
+  //   // For checkouts such as draft order invoices, cart attributes may not be allowed
+  //   // Consider rendering a fallback UI or nothing at all, if the feature is unavailable
+  //   return (
+  //     <Banner title="Custom SMS Consent" status="warning">
+  //       {translate("attributeChangesAreNotSupported")}
+  //     </Banner>
+  //   );
+  // }
+
+  // // 3. Render a UI
+  // return (
+  //   <BlockStack border={"dotted"} padding={"tight"}>
+  //     <Banner title="Custom SMS Consent">
+  //       {translate("welcome", {
+  //         target: <Text emphasis="italic">{extension.target}</Text>,
+  //       })}
+  //     </Banner>
+  //     <Checkbox onChange={onCheckboxChange}>
+  //       {translate("iWouldLikeAFreeGiftWithMyOrder")}
+  //     </Checkbox>
+  //   </BlockStack>
+  // );
+
+  // async function onCheckboxChange(isChecked) {
+  //   // 4. Call the API to modify checkout
+  //   const result = await applyAttributeChange({
+  //     key: "requestedFreeGift",
+  //     type: "updateAttribute",
+  //     value: isChecked ? "yes" : "no",
+  //   });
+  //   console.log("applyAttributeChange result", result);
+  // }
 }
