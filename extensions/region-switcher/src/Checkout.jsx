590ce4a9620f30175f6f7a8cf50058661bf3746c
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-09-11 01:32:53 +1000

    output links from array of options

--- extensions/region-switcher/src/Checkout.jsx
+++ extensions/region-switcher/src/Checkout.jsx
@@ -1,59 +1,46 @@
 import {
   reactExtension,
-  Banner,
-  BlockStack,
-  Checkbox,
-  Text,
-  useApi,
-  useApplyAttributeChange,
-  useInstructions,
-  useTranslate,
+  Link,
 } from "@shopify/ui-extensions-react/checkout";
 
+const SWITCHER_OPTIONS = [
+  {
+    name: 'AUS & NZ',
+    domain: 'whitefoxboutique.com.au',
+  },
+  {
+    name: 'USA, CAD, MEX',
+    domain: 'whitefoxboutique.com',
+  },
+  {
+    name: 'EUROPE & UAE',
+    domain: 'whitefoxboutique.co.uk',
+    params: { currency: 'EUR' },
+  },
+  {
+    name: 'UK',
+    domain: 'whitefoxboutique.co.uk',
+    params: { currency: 'GBP' },
+  },
+  {
+    name: 'REST OF THE WORLD',
+    domain: 'whitefoxboutique.com.au',
+  },
+];
+
 // 1. Choose an extension target
-export default reactExtension("purchase.checkout.block.render", () => (
+export default reactExtension('purchase.checkout.footer.render-after', () => (
   <Extension />
 ));
 
 function Extension() {
-  const translate = useTranslate();
-  const { extension } = useApi();
-  const instructions = useInstructions();
-  const applyAttributeChange = useApplyAttributeChange();
-
-
-  // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
-  if (!instructions.attributes.canUpdateAttributes) {
-    // For checkouts such as draft order invoices, cart attributes may not be allowed
-    // Consider rendering a fallback UI or nothing at all, if the feature is unavailable
-    return (
-      <Banner title="region-switcher" status="warning">
-        {translate("attributeChangesAreNotSupported")}
-      </Banner>
-    );
-  }
-
-  // 3. Render a UI
   return (
-    <BlockStack border={"dotted"} padding={"tight"}>
-      <Banner title="region-switcher">
-        {translate("welcome", {
-          target: <Text emphasis="italic">{extension.target}</Text>,
-        })}
-      </Banner>
-      <Checkbox onChange={onCheckboxChange}>
-        {translate("iWouldLikeAFreeGiftWithMyOrder")}
-      </Checkbox>
-    </BlockStack>
+    <>
+      { SWITCHER_OPTIONS.map(option => {
+        const { name, domain, params } = option;
+        const url = `https://${ domain }${ params ? new URLSearchParams(params).toString() : '' }`;
+        return <Link to={ url }>{ name }</Link>;
+      }) }
+    </>
   );
-
-  async function onCheckboxChange(isChecked) {
-    // 4. Call the API to modify checkout
-    const result = await applyAttributeChange({
-      key: "requestedFreeGift",
-      type: "updateAttribute",
-      value: isChecked ? "yes" : "no",
-    });
-    console.log("applyAttributeChange result", result);
-  }
 }
