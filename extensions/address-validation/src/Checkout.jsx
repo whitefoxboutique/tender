b60de279827a2854af8240e3ce15d9470a07438d
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-10-16 17:47:03 +1100

    fix issues with blocking demo

--- extensions/address-validation/src/Checkout.jsx
+++ extensions/address-validation/src/Checkout.jsx
@@ -1,6 +1,7 @@
 import {
   reactExtension,
   useExtensionCapability,
+  useBuyerJourneyIntercept,
 } from '@shopify/ui-extensions-react/checkout';
 
 // 1. Choose an extension target
@@ -11,17 +12,29 @@ export default reactExtension('purchase.checkout.delivery-address.render-before'
 function Extension() {
   const canBlockProgress = useExtensionCapability('block_progress');
 
-  if (canBlockProgress) {
-    return {
-      behavior: 'block',
-      reason: `Just don't like u`,
-      errors: [
-        {
-          message: `I think I'm just tired.`,
-        },
-      ],
-    };  
-  }
+  console.log('canBlockProgress', canBlockProgress);
+
+  useBuyerJourneyIntercept(({ canBlockProgress }) => {
+    if (canBlockProgress) {
+      console.log('block');
+      return {
+        behavior: 'block',
+        reason: `Just don't like u`,
+        errors: [
+          {
+            message: `I think I'm just tired`,
+            // Show an error underneath the country code field
+            target: '$.cart.deliveryGroups[0].deliveryAddress.countryCode',
+          },
+          {
+            // In addition, show an error at the page level
+            message: `I don't like the cut of your jib.`,
+          },
+        ],
+      };
+    }
+  });
   
+  console.log(`Don't block`);
   return;
 }
