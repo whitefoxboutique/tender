28b3942be6e9ddd927e8729391958980ba2c10ea
John <29110105+GorgonFreeman@users.noreply.github.com>
2024-10-24 15:36:47 +1100

    #86cwvpuju - migrate changes back to original extension

--- extensions/address-validation/src/Checkout.jsx
+++ extensions/address-validation/src/Checkout.jsx
@@ -3,6 +3,7 @@ import {
   useExtensionCapability,
   useBuyerJourneyIntercept,
   useShippingAddress,
+  useTranslate,
 } from '@shopify/ui-extensions-react/checkout';
 
 // 1. Choose an extension target
@@ -10,31 +11,53 @@ export default reactExtension('purchase.checkout.delivery-address.render-before'
   <Extension />
 ));
 
+const FIELD_LIMITS = {
+  address1: 40,
+  address2: 40,
+  company: 40,
+  city: 20,
+};
+
 function Extension() {
 
+  const translate = useTranslate();
   const shippingAddress = useShippingAddress();
-  const { address1 } = shippingAddress;
-  const address1IsTooLong = address1.length > 40;
-
   const canBlockProgress = useExtensionCapability('block_progress');
-
   console.log('canBlockProgress', canBlockProgress);
 
+  const blockingErrors = [];
+
+  for (const [fieldName, limit] of Object.entries(FIELD_LIMITS)) {
+
+    const fieldValue = shippingAddress?.[fieldName];
+
+    if (!fieldValue) {
+      continue;
+    }
+
+    if (fieldValue.length > limit) {
+
+      const blockingError = {
+        message: `${ translate(`${ fieldName }IsTooLong`) } ${ fieldValue.length }/${ limit }`,
+        // Show an error underneath the specific address field
+        target: `$.cart.deliveryGroups[0].deliveryAddress.${ fieldName }`,
+      };
+
+      blockingErrors.push(blockingError);
+    }
+  }
+
   useBuyerJourneyIntercept(({ canBlockProgress }) => {
-    if (canBlockProgress && address1IsTooLong) {
+    if (canBlockProgress && blockingErrors?.length) {
       console.log('block');
       return {
         behavior: 'block',
-        reason: `Address1 cannot be longer than 40 characters.`,
+        reason: translate('tooLongErrorMessage'),
         errors: [
-          {
-            message: `Please use 40 characters or less. ${ address1.length }/40`,
-            // Show an error underneath the country code field
-            target: '$.cart.deliveryGroups[0].deliveryAddress.address1',
-          },
+          ...blockingErrors,
           {
             // In addition, show an error at the page level
-            message: `Our shipping company has max character limits - please edit your address.`,
+            message: translate('tooLongErrorMessage'),
           },
         ],
       };
